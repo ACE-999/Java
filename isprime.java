8204b143e1e94c9571f1fb7c3c2c59f631f8141e
Aditya Prakash <adi2000prakash@gmail.com>
2022-08-23 18:14:39 +0530

    new

--- isprime.java
+++ isprime.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class isprime {
     public static boolean isPrime(int n) {
 
+        if(n<=1)
+        {
+            return false;
+        }
+        
         for(int i=2; i*i<=n; i++)
         {
             if(n%i==0)
