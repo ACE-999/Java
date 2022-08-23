e5515baac3229ddd901e05e07f7245687bbe2441
Aditya Prakash <adi2000prakash@gmail.com>
2022-08-23 17:32:40 +0530

    prime

---
+++ isprime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class isprime {
+    public static boolean isPrime(int n) {
+
+        for(int i=2; i*i<=n; i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int n=sc.nextInt();
+
+        if(isPrime(n))
+        {
+            System.out.println("The number is Prime!!!");
+        }
+        else{
+            System.out.println("The number is Not Prime!!!");
+        }
+
+        sc.close();
+    }
+    
+}
