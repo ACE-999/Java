5834d22567306808972a29cf0ecb5628b1119d95
Aditya Prakash <adi2000prakash@gmail.com>
2022-09-05 01:10:29 +0530

    recursion

---
+++ Recursion/gcdrecur.java
@@ -0,0 +1,33 @@
+package Recursion;
+import java.util.Scanner;
+
+public class gcdrecur {
+    
+    public static int gcd(int a, int b)
+    {
+        if(a==0)
+            return b;
+        else if(b==0)
+            return a;
+        else if(a==b)
+            return a;
+        else if(a>b)
+        {
+            return gcd(a-b,b);
+        }
+        return gcd(a,b-a);
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int res=gcd(a,b);
+        System.out.println("GCD: "+res);
+
+        sc.close();
+    }
+    
+}
