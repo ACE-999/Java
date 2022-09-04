bed05a1c3136d66f5ad5af317eeb0978a4eaedee
Aditya Prakash <adi2000prakash@gmail.com>
2022-09-05 00:08:16 +0530

    update

--- cntprime.java
+++ cntprime.java
@@ -23,7 +23,7 @@ public class cntprime {
         
         int cnt=0;
         
-        for(int i=0; i<n; i++)
+        for(int i=2; i<=n; i++)
         {
             if(isPrime(i))
             {
