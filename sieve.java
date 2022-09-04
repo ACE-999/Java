5c0be4522a6d2d22f4203f0f0b87be7d3aa268c7
Aditya Prakash <adi2000prakash@gmail.com>
2022-09-05 00:13:25 +0530

    update

--- sieve.java
+++ sieve.java
@@ -6,20 +6,20 @@ public class sieve {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        boolean prime[]=new boolean[n];
+        boolean prime[]=new boolean[n+1];
         int cnt=0;
 
         //all non primes are true
         for(int i=2; i*i<=n; i++)
         {
-            for(int j=i*i; j<n; j+=i)
+            for(int j=i+i; j<=n; j+=i)
             {
                 prime[j]=true;
             }
         }
 
         //all non primes are false
-        for(int i=2; i<n; i++)
+        for(int i=2; i<=n; i++)
         {
 
             if(prime[i]==false)
